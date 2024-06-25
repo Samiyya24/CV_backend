277bc84718c42eb4f5d625913211411389a3a374
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-06-25 07:47:01 +0500

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      host: process.env.POSTGRES_HOST,
+      synchronize: true,
+      entities: [],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
-async function start() {
+const start = async () => {
   try {
-  } catch (error) {}
-}
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    const PORT = configService.get<number>('PORT') || 3000;
+
+    app.setGlobalPrefix('api');
+    await app.listen(PORT, () => {
+      console.log(`Server started at ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting server:', error);
+  }
+};
+
 start();
