c5212b9d13303797eb7ee05ebee0d9040a9def87
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-06-25 09:57:39 +0500

    added user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
       entities: [],
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
